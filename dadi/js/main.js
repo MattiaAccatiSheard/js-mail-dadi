448459a3902ace7e576524f7d35cf110c3752fa7
MattiaAccatiSheard <mattiahtsheard@gmail.com>
2023-01-30 02:48:36 +0100

    .dadi

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -5,9 +5,17 @@ generateN.addEventListener(
 
     function () {
         let pcN = Math.floor(Math.random() * 6) + 1;
-        let userN = Math.floor(Math.random() * 6) + 1;
-        console.log(userN);
-        console.log(pcN);
-        
+        const userN = Math.floor(Math.random() * 6) + 1;
+    
+        if (pcN > userN) {
+            //output
+            document.getElementById('risultato').innerHTML = `Hai perso! Il pc ha fatto ${pcN} e tu ${userN}`;
+        } else if (pcN == userN) {
+            //output
+            document.getElementById('risultato').innerHTML = `Hai vinto! Il pc ha fatto ${pcN} e tu ${userN}`;
+        } else {
+            //output
+            document.getElementById('risultato').innerHTML = `Pareggio. Il numero uscito a te ed al pc sono ${pcN}`;
+        }        
     }
 )
