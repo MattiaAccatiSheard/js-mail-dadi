23007d7f019dca7a33786b40f151ce6d9420c070
MattiaAccatiSheard <mattiahtsheard@gmail.com>
2023-01-31 00:16:07 +0100

    .js1

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,1 +1,10 @@
+let btn = document.getElementById('btn');
+let mailValue = document.getElementById('usMail').value;
+let arr = ['bello@gmail.com', 'tenero@gmail.com', 'rozzo@gmail.com', 'stanco@gmail.com'];
 
+
+btn.addEventListener=(
+    'click',
+    //variabili utili
+
+);
